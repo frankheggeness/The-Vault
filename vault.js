5b1efed7b4d0de846b55956eb26c9f02ec8f00ee
Frank <fr.heggeness@gmail.com>
2019-04-01 19:17:08 -1000

    all tests

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
-  
+  let myVault = {};
+  function setValue(key,value){
+    if(!key || !value){
+      return null;
+    }
+      myVault.key = value;
+  }
+  function getValue(key){
+    if(myVault.key){
+      return myVault.key;
+    }else{
+      return null;
+    }
+  }
+  return{
+    setValue: setValue,
+    getValue: getValue
+    
+  }
 };
